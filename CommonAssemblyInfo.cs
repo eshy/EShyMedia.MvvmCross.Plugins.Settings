ed449db6bfd1386c9ff9a95ab8655d693261b335
Ishai Hachlili <ishai@ishaihachlili.com>
2016-07-08 19:23:13 -0700

    added missing common asssmebly info file

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,29 @@
+using System.Reflection;
+using System.Resources;
+
+
+#if DEBUG
+[assembly: AssemblyConfiguration("DEBUG")]
+#else
+[assembly: AssemblyConfiguration("")]
+#endif
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("Ishai Hachlili")]
+[assembly: AssemblyProduct("EShyMedia.MvvmCross.Plugins.Settings")]
+[assembly: AssemblyCopyright("Copyright ©  2013-2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.5.0")]
+[assembly: AssemblyFileVersion("2.0.5.0")]
