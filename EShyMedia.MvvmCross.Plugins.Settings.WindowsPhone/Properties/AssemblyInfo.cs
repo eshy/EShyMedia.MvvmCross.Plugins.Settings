487d1214bf63ee289b466bdc2ff5a49461a8ea9f
Ishai Hachlili <ishai@ishaihachlili.com>
2016-07-08 18:48:11 -0700

    added common assembly info class

--- EShyMedia.MvvmCross.Plugins.Settings.WindowsPhone/Properties/AssemblyInfo.cs
+++ EShyMedia.MvvmCross.Plugins.Settings.WindowsPhone/Properties/AssemblyInfo.cs
@@ -1,18 +1,10 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EShyMedia.MvvmCross.Plugins.Settings.WindowsPhone")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EShyMedia.MvvmCross.Plugins.Settings.WindowsPhone")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -22,15 +14,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("07f5b7dd-2f46-4da9-8e2f-c027e7024241")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Revision and Build Numbers 
-// by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("en-US")]
